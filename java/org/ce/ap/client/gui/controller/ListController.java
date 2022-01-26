a4d6be00b2f6247e3e6e188d5ae746ee1f53592b
empahmadi <emp.ahmadi@gmail.com>
2022-01-26 09:26:53 +0330

    [feature] list page toggle screen and mode enabled.

--- java/org/ce/ap/client/gui/controller/ListController.java
+++ java/org/ce/ap/client/gui/controller/ListController.java
@@ -11,6 +11,8 @@ import javafx.scene.layout.VBox;
 import org.ce.ap.client.pages.Main;
 import org.json.JSONArray;
 
+import java.util.ArrayList;
+
 /**
  * this class is a controller for list page.
  *
@@ -25,6 +27,7 @@ public class ListController {
     private ScrollPane parent;
     @FXML
     private VBox list;
+    private ArrayList<HBox> lists;
 
     /**
      * this method init our requirement for a list.
@@ -37,43 +40,32 @@ public class ListController {
     public void init(int size, String mode, JSONArray items, Main main) {
         this.main = main;
         notFound.setVisible(false);
-        if (size == 0) {
-            list.getStyleClass().add("list-s");
-            parent.getStyleClass().add("parent-s");
-        } else {
-            list.getStyleClass().add("list-l");
-            parent.getStyleClass().add("parent-l");
-        }
+        toggleScreen(size);
+        lists = new ArrayList<>();
         if (items.length() == 0) {
             notFound.setVisible(true);
         } else {
             for (Object i : items) {
-                list.getChildren().add(createItem(size, mode, (String) i));
+                list.getChildren().add(createItem(mode, (String) i));
             }
         }
+        toggleTheme(mode);
     }
 
     /**
      * create an item for our list.
      *
-     * @param size .
      * @param mode .
      * @param user .
      * @return item of list
      */
-    private HBox createItem(int size, String mode, String user) {
+    private HBox createItem(String mode, String user) {
         HBox item = new HBox();
         Button username = new Button(user);
-        username.getStyleClass().add("username");
-        if (size == 0) {
-            item.getStyleClass().add("item-s");
-        } else {
-            item.getStyleClass().add("item-l");
-        }
-        item.getStyleClass().add(mode);
-        username.getStyleClass().add(mode);
-
         item.getChildren().add(username);
+        username.getStyleClass().add("username");
+        item.getStyleClass().add("item");
+        lists.add(item);
         item.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
@@ -83,11 +75,23 @@ public class ListController {
         return item;
     }
 
-    public void toggleScreen(int size){
-
+    public void toggleScreen(int size) {
+        if (size == 0) {
+            list.getStyleClass().add("list-s");
+            parent.getStyleClass().add("content-s");
+        } else {
+            list.getStyleClass().add("list-l");
+            parent.getStyleClass().add("content-l");
+        }
     }
 
-    public void toggleTheme(String mode){
-
+    public void toggleTheme(String mode) {
+        list.getStyleClass().add(mode);
+        parent.getStyleClass().add(mode);
+        for (HBox i: lists){
+            i.getStyleClass().add(mode);
+            i.getChildren().get(0).getStyleClass().add(mode);
+        }
+        notFound.getStyleClass().add(mode);
     }
 }
