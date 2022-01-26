4886cb744dad58acf5e29e65041b851778fdcdf6
empahmadi <emp.ahmadi@gmail.com>
2022-01-26 07:09:12 +0330

    [feature] timeline screen and theme mode toggle is ready to use.

--- java/org/ce/ap/client/gui/controller/TimeLineController.java
+++ java/org/ce/ap/client/gui/controller/TimeLineController.java
@@ -8,17 +8,30 @@ import org.ce.ap.client.pages.Main;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+/**
+ * this class is a controller for timeline page.
+ *
+ * @author Eid Mohammad Ahmadi
+ * @version 1.0
+ */
 public class TimeLineController {
     @FXML
     private ScrollPane parent;
     @FXML
     private VBox list;
 
-    public void init(int size, String mode, JSONArray tweets, Main main){
-        for (Object i: tweets){
+    /**
+     * init our page.
+     *
+     * @param size   .
+     * @param mode   .
+     * @param tweets .
+     * @param main   .
+     */
+    public void init(int size, String mode, JSONArray tweets, Main main) {
+        for (Object i : tweets) {
             list.getChildren().add(main.getTweet((JSONObject) i));
         }
-        main.incrementIndex();
         Label last = new Label();
         last.setMaxHeight(100);
         last.setMinHeight(100);
@@ -27,15 +40,32 @@ public class TimeLineController {
         last.setMinWidth(100);
         last.setPrefWidth(100);
         list.getChildren().add(last);
-
+        toggleScreen(size);
+        toggleTheme(mode);
     }
 
-
-    public void toggleScreen(int size){
-
+    /**
+     * change the size of screen.
+     *
+     * @param size .
+     */
+    public void toggleScreen(int size) {
+        if (size == 0) {
+            parent.getStyleClass().add("content-s");
+            list.getStyleClass().add("list-s");
+        } else {
+            parent.getStyleClass().add("content-l");
+            list.getStyleClass().add("list-l");
+        }
     }
 
-    public void toggleTheme(String mode){
-
+    /**
+     * change the mode of screen.
+     *
+     * @param mode .
+     */
+    public void toggleTheme(String mode) {
+        parent.getStyleClass().add(mode);
+        list.getStyleClass().add(mode);
     }
 }
